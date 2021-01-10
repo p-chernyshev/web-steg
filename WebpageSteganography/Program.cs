2663e7390ceb3c7afc76ad314ed6c533f6c56136
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2021-01-10 16:44:43 +0500

    Parse documents

--- WebpageSteganography/Program.cs
+++ WebpageSteganography/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,244 @@ namespace WebpageSteganography
         static void Main(string[] args)
         {
         }
+    }
+
+    interface DocumentPart
+    {
+
+    }
+
+    class DocumentBlock : DocumentPart
+    {
+        protected DocumentLine[] Lines;
+        public int Length => Lines.Length;
+    }
+
+    class DocumentLine : DocumentPart
+    {
+        protected string RawLine;
+        protected string LineContent;
+        bool IsEmpty => LineContent.Length == 0;
+        static string CollapseWhitespace(string line)
+        {
+            string[] parts = line.Trim().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            //var nonEmptyParts = parts.Where(part => part.Length != 0);
+            return string.Join(" ", parts);
+        }
+
+        private DocumentLine() { }
+        public DocumentLine(string line)
+        {
+            RawLine = line;
+            LineContent = CollapseWhitespace(line);
+        }
+    }
+
+    class CssRuleBlock : DocumentBlock
+    {
+        //nocheck
+        //no empty lines
+        public CssRuleBlock(string[] lines)
+        {
+            DocumentLine[] selectors = lines
+                .TakeWhile(CssSelectorLine.CanParse)
+                .Select(line => new CssSelectorLine(line))
+                .ToArray();
+            DocumentLine[] properties = lines
+                .Skip(selectors.Length)
+                .TakeWhile(line => !CssClosingBraceLine.CanParse(line))
+                .Select(line => new CssPropertyLine(line))
+                .ToArray();
+
+            string closingBraceLine = lines.Skip(selectors.Length + properties.Length).First();
+            DocumentLine closingBrace = new CssClosingBraceLine(closingBraceLine);
+
+            Lines = selectors.Concat(properties).Append(closingBrace).ToArray();
+        }
+    }
+
+    class CssSelectorLine : DocumentLine
+    {
+        //bool Last;
+        public static bool CanParse(string line)
+        {
+            line = line.Trim();
+            return line.Last() == ',' || line.Last() == '{';
+        }
+        public CssSelectorLine(string line) : base(line) { }
+
+    }
+    class CssPropertyLine : DocumentLine
+    {
+        string Key;
+        string Value;
+        public CssPropertyLine(string line) : base(line) {
+            line = LineContent.TrimEnd(';');
+            int colonPosition = line.IndexOf(':');
+            Key = line.Substring(0, colonPosition);
+            Value = line.Substring(colonPosition + 1);
+        }
+    }
+    class CssClosingBraceLine : DocumentLine
+    {
+        public static bool CanParse(string line)
+        {
+            return line.Trim() == "}";
+        }
+        public CssClosingBraceLine(string line) : base(line) { }
+    }
+
+    class HtmlBodyBlock : DocumentBlock
+    {
+        public HtmlBodyBlock(DocumentLine[] lines)
+        {
+            Lines = lines;
+        }
+    }
+
+    class HtmlOpeningTagLine : DocumentLine
+    {
+        struct Attribute
+        {
+            string Key;
+            string Value;
+            public Attribute(string key, string value)
+            {
+                Key = key;
+                Value = value;
+            }
+        }
+
+        public readonly string Name;
+        Attribute[] Attributes;
+        public static bool CanParse(string line)
+        {
+            line = line.Trim();
+            return line[0] == '<' && line[1] != '/';
+        }
+        public HtmlOpeningTagLine(string line) : base(line) {
+            line = LineContent.Trim(new[] { '<', '>' });
+
+            string[] parts = line.Split();
+
+            Name = parts[0];
+
+            Attributes = parts
+                .Skip(1)
+                .Select(attributePart =>
+                {
+                    string[] attributeKeyValue = attributePart.Split(new[] { '=', ' ', '\'', '"' }, 2, StringSplitOptions.RemoveEmptyEntries); // check
+                    return new Attribute(attributeKeyValue[0], attributeKeyValue[1]);
+                })
+                .ToArray();
+        }
+    }
+    class HtmlClosingTagLine : DocumentLine
+    {
+        public readonly string Name;
+        public static bool CanParse(string line)
+        {
+            line = line.Trim();
+            return line[0] == '<' && line[1] == '/';
+        }
+        public HtmlClosingTagLine(string line) : base(line)
+        {
+            Name = LineContent.Trim(new[] { '<', '>', '/' });
+        }
+    }
+    //class HtmlTextLine : DocumentLine
+    //{
+    //    public HtmlTextLine(string line) : base(line) { }
+    //}
+
+    abstract class Document
+    {
+        DocumentPart[] parts;
+        public Document(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                string[] lines = File.ReadAllLines(fileName, Encoding.UTF8); // CSS??
+                //lines = TrimWhitespace(lines);
+                parts = ParseLines(lines);
+            }
+            else
+            {
+                throw new FileNotFoundException($"File '{fileName}' not found");
+            }
+        }
+
+        //string[] TrimWhitespace(string[] lines)
+        //{
+        //    return lines
+        //        .Select(TrimWhitespace)
+        //        .Where(line => line.Length != 0)
+        //        .ToArray();
+        //}
+
+        abstract protected DocumentPart[] ParseLines(string[] lines);
+    }
+
+    class Html : Document
+    {
+        public Html(string fileName) : base(fileName) { }
+
+        override protected DocumentPart[] ParseLines(string[] lines)
+        {
+            List<DocumentLine> documentLines = new List<DocumentLine>();
+
+            int bodyStartIndex = 0, bodyEndIndex = 0;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                DocumentLine documentLine;
+
+                if (HtmlOpeningTagLine.CanParse(line))
+                {
+                    HtmlOpeningTagLine openingTag = new HtmlOpeningTagLine(line);
+                    documentLine = openingTag;
+                    if (openingTag.Name == "body") bodyStartIndex = i;
+                } else if (HtmlClosingTagLine.CanParse(line))
+                {
+                    HtmlClosingTagLine closingTag = new HtmlClosingTagLine(line);
+                    documentLine = closingTag;
+                    if (closingTag.Name == "body") bodyEndIndex = i;
+                } else
+                {
+                    documentLine = new DocumentLine(line);
+                }
+
+                documentLines.Add(documentLine);
+            }
+
+            HtmlBodyBlock body = new HtmlBodyBlock(documentLines.GetRange(bodyStartIndex, bodyEndIndex - bodyStartIndex + 1).ToArray());
+
+            List<DocumentPart> parts = documentLines.OfType<DocumentPart>().ToList();
+
+            parts.RemoveRange(bodyStartIndex, bodyEndIndex - bodyStartIndex + 1);
+            parts.Insert(bodyStartIndex, body);
+
+            return parts.ToArray();
+        }
+    }
+
+    class Css : Document
+    {
+        public Css(string fileName) : base(fileName) { }
+        override protected DocumentPart[] ParseLines(string[] lines)
+        {
+            List<DocumentPart> parts = new List<DocumentPart>();
+
+            while (lines.Length != 0)
+            {
+                CssRuleBlock block = new CssRuleBlock(lines);
+                parts.Add(block);
+                lines = lines.Skip(block.Length).ToArray();
+            }
+            
+            return parts.ToArray();
+        }
+
     }
 }
